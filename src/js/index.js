8203123282765358e34964d8fd2ab7097b5871c6
Maite <maipeca@hotmail.com>
2022-11-22 01:56:47 +0100

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 
-
+anime({
+    targets: '#amazing path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function(el, i) { return i * 500 },
+    direction: 'alternate',
+    loop: false
+});
